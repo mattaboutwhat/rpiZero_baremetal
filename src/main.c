360af609a02a10594ff5cb809e74de3317bf3668
Matthew A <mellifluentmatt@gmail.com>
2016-05-09 20:10:00 -0400

    systimer-based STAT LED blinking

--- src/main.c
+++ src/main.c
@@ -1,21 +1,23 @@
-#include <stdlib.h>
+//#include <stdlib.h>
 
 #include "base.h"
 #include "gpio.h"
+#include "systimer.h"
+//#include "armtimer.h"
+//#include "interrupts.h"
 
+extern void _enable_interrupts(void);
 
 void kernel_main(unsigned int r0, unsigned int r1, unsigned int atags)
-{
-	//led GPIO output config
+{		
+	/** Configure LED GPIO output and turn on **/
 	getGPIOController()->LED_FSEL |= (1 << LED_FSELBIT);
-
-	//led OFF
-	getGPIOController()->LED_SET = (1 << LED_GPIOBIT);
+	getGPIOController()->LED_CLR = (1<<LED_GPIOBIT);
 
 	while(1) {
-		for (int i=0; i<500000; i++);
-		getGPIOController()->LED_CLR = (1 << LED_GPIOBIT);
-		for (int i=0; i<500000; i++);
-		getGPIOController()->LED_SET = (1 << LED_GPIOBIT);
+		WaitMicroSeconds(500000);
+		getGPIOController()->LED_SET = (1<<LED_GPIOBIT);
+		WaitMicroSeconds(500000);
+		getGPIOController()->LED_CLR = (1<<LED_GPIOBIT);		
 	}
 }
